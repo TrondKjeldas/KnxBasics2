e9e76fc04c7aa44793a4eaa130aac0c459389e99
Trond Kjeldås <trond@kjeldas.no>
2016-04-28 19:30:22 +0200

    Added Documentation to version control

--- doc_generation.sh
+++ doc_generation.sh
@@ -1,1 +1,1 @@
-jazzy --theme fullwidth --author Trond Kjeldås --skip-undocumented --module KnxBasics2 --swift-version 2.2
+jazzy --output Documentation --theme fullwidth --author Trond Kjeldås --skip-undocumented --module KnxBasics2 --swift-version 2.2
