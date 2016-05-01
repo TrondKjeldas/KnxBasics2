3b644060aacfa7eb224988b99daaffbfe87a61ae
Trond Kjeldås <trond@kjeldas.no>
2016-05-01 11:12:07 +0200

    Added LGPL V2.1 license notice.

--- KnxRouterInterface.playground/Contents.swift
+++ KnxRouterInterface.playground/Contents.swift
@@ -1,3 +1,20 @@
+//
+//  The KnxBasics2 framework provides basic interworking with a KNX installation.
+//  Copyright © 2016 Trond Kjeldås (trond@kjeldas.no).
+//
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License Version 2.1
+//  as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
 //: Playground - noun: a place where people can play
 
 import XCPlayground
--- Source/KnxResponseHandlerDelegate.swift
+++ Source/KnxResponseHandlerDelegate.swift
@@ -2,8 +2,22 @@
 //  KnxResponseHandlerDelegate.swift
 //  KnxBasics2
 //
-//  Created by Trond Kjeldås on 23/04/16.
-//  Copyright © 2016 Trond Kjeldås. All rights reserved.
+//
+//  The KnxBasics2 framework provides basic interworking with a KNX installation.
+//  Copyright © 2016 Trond Kjeldås (trond@kjeldas.no).
+//
+//  This library is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License Version 2.1
+//  as published by the Free Software Foundation.
+//
+//  This library is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU Lesser General Public License for more details.
+//
+//  You should have received a copy of the GNU Lesser General Public License
+//  along with this library; if not, write to the Free Software
+//  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 //
 
 import Foundation
